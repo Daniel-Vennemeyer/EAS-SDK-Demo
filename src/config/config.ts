2f693541b2f4e75535b7fa2f1004ade843960c3c
Daniel Vennemeyer <77549269+Daniel-Vennemeyer@users.noreply.github.com>
2024-07-10 09:21:11 -0400

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,7 @@
 export const FREELANCER_ADDRESS = import.meta.env.VITE_FREELANCER_ADDRESS;
 export const CLIENT_ADDRESS = import.meta.env.VITE_CLIENT_ADDRESS;
 // eas-sdk config
+export const ALCHEMY_API_KEY = ""
 export const EAS_ADDRESS = "0xC2679fBD37d54388Ce493F1DB75320D236e1815e"; // Sepolia v0.26
 export const SCHEMA_REGISTRY_ADDRESS =
   "0x0a7E2Ff54e76B8E6659aedc9103FB21c038050D0"; // Sepolia
